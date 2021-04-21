f3823ebff7376ee0dc5f51390759bf881bbad532
Spencer Kotys <58829765+Spencer-Kotys@users.noreply.github.com>
2021-04-21 00:08:54 -0400

    Create EVENT.C

---
+++ Chapter2/EVENT.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(void)
+   {
+   int event;
+   char heat;
+   float time;
+
+   event = 5;
+   heat = 'C';
+   time = 27.25;
+   printf("\nThe winning time in head %c", heat);
+   printf(" of event %d was %f.", event, time);
+   }
